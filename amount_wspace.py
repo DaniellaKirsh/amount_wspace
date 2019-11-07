11c7538d974c0e230e27ec62997b07fe7fb1fb35
Daniella Kirshenbaum <daniella.k@vatbox.com>
2019-11-07 14:41:41 +0200

    first commit

---
+++ amount_wspace.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.width', 320)
+
+amount_df = pd.read_csv('Amount_Details_crosstab.csv', sep='\t', encoding='utf-16le')
+amount_df.dropna(axis=1, inplace=True)
+print(amount_df.head())
+
+val = 1
